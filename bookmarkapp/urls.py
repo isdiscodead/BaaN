b655a9874e4e232c111eb165108bb828cbddfc5d
isdiscodead <isdiscodead@korea.ac.kr>
2021-07-24 20:39:22 +0900

    add favicon

--- bookmarkapp/urls.py
+++ bookmarkapp/urls.py
@@ -3,8 +3,11 @@ from django.contrib.auth import admin
 from django.urls import path
 
 from bookmarkapp import views
+from bookmarkapp.views import bookmarkListView
 
 app_name = 'bookmarkapp'
 
 urlpatterns = [
+    path('list/', bookmarkListView.as_view(template_name='bookmarkapp/list.html'), name='list'),
+
 ]
--- bookmarkapp/views.py
+++ bookmarkapp/views.py
@@ -1,10 +1,17 @@
 # from django.shortcuts import render
 #
 # # Create your views here.
-# from bookmarkapp.models import Bookmark
+from django.views.generic import ListView
+
+from bookmarkapp.models import Bookmark
 #
 # def home(request):
 #     urlList = Bookmark.objects.order_by("title")
 #     urlCount = Bookmark.objects.all().count()
 #
 #     return render("list.html", {"urlList":urlList, "urlCount":urlCount})
+
+class bookmarkListView(ListView):
+    model = Bookmark
+    context_object_name = 'bookmark_list'
+    template_name = 'bookmarkapp/list.html'
