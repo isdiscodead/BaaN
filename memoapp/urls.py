7cb02680f82a93d8415213ec0ac77e6612ea6da6
witway <witway@korea.ac.kr>
2021-08-12 10:10:47 +0900

    depressed memoapp2..

--- memoapp/urls.py
+++ memoapp/urls.py
@@ -2,18 +2,16 @@ from django.conf.urls import url
 from django.urls import path
 
 from memoapp import views
-from memoapp.views import MemoDetailView, MemoDeleteView, MemoListView, MemoUpdateView
-
-# MemoListView, MemoCreateView, MemoDetailView, MemoUpdateView, MemoDeleteView
+from memoapp.views import MemoDetailView, MemoListView, MemoUpdateView, MemoCreateView
 
 app_name = 'memoapp'
 
 
 urlpatterns = [
     path('list/', MemoListView.as_view(), name='list'),
-    path('create/', views.memo_create, name='create'),
-    # path('create/', MemoCreateView.as_view(), name='create'),
-    path('detail/<int:pk>', views.MemoDetailView, name='detail'),
+    # path('create/', views.memo_create, name='create'),
+    path('create/', MemoCreateView.as_view(), name='create'),
+    path('detail/<int:pk>', MemoDetailView.as_view(), name='detail'),
     path('update/<int:pk>', MemoUpdateView.as_view(), name='update'),
     path('delete/<int:pk>', views.memo_delete, name='delete'),
 ]
--- memoapp/views.py
+++ memoapp/views.py
@@ -2,28 +2,36 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.urls import reverse, reverse_lazy
-from django.views.generic import DetailView, DeleteView, ListView, UpdateView
+from django.utils.decorators import method_decorator
+from django.views.generic import DetailView, ListView, UpdateView, CreateView
 
 from memoapp.forms import MemoForm
 from memoapp.models import Memo
 
-def memo_create(request):
-    form = MemoForm(request.POST)
-    if form.is_valid():
-        title = request.POST.get('title')
-        content = request.POST.get('content')
-        user = request.user
-        return redirect("home")
-    return render(request, "snippets/memo_create.html", {"form": form})
+# def memo_create(request):
+#     form = MemoForm(request.POST)
+#     if form.is_valid():
+#         title = request.POST.get('title')
+#         content = request.POST.get('content')
+#         user = request.user
+#         return redirect("home")
+#     return render(request, "memoapp/create.html", {"form": form})
 
+@method_decorator(login_required(login_url='/accounts/login/'), 'get')
+@method_decorator(login_required(login_url='/accounts/login/'), 'post')
+class MemoCreateView(CreateView):
+    model = Memo
+    form_class = MemoForm
+    template_name = 'memoapp/create.html'
 
-# class MemoCreateView(CreateView):
-#     model = Memo
-#     form_class = MemoForm
-#     # template_name = 'snippets/memo_create.html'
-#
-#     def get_success_url(self):
-#         return reverse('memoapp:list')
+    def form_valid(self, form):
+        temp_memo = form.save(commit=False)
+        temp_memo.user = self.request.user
+        temp_memo.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('memoapp:list')
 
 class MemoUpdateView(UpdateView):
     model = Memo
@@ -39,12 +47,6 @@ class MemoDetailView(DetailView):
     form_class = MemoForm
     context_object_name = 'target_memo'
     template_name = 'memoapp/detail.html'
-
-class MemoDeleteView(DeleteView):
-    model = Memo
-    context_object_name = 'target_memo'
-    success_url = reverse_lazy('memoapp:list')
-    template_name = 'memoapp/delete.html'
 
 class MemoListView(ListView):
     model = Memo
