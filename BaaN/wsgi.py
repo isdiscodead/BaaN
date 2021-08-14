18dee05ecdf23b5e0e812edae48370fb0fb30eac
hyeonsu8241 <83970378+hyeonsu8241@users.noreply.github.com>
2021-08-14 11:24:41 +0900

    Update wsgi.py

--- BaaN/wsgi.py
+++ BaaN/wsgi.py
@@ -11,6 +11,11 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+path = os.path.abspath(__file__+'/../..')
+
+if path not in sys.path:
+    sys.path.append(path)
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BaaN.settings')
 
 application = get_wsgi_application()
